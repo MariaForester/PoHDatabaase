28a55ab298d71efd0b2ed37f4a7053eeedee404e
Amario Forester <maschalessnichaya@gmail.com>
2018-06-26 21:17:55 +0300

    firebase, text view in hero activity

--- app/src/main/java/com/poh/base/Hero_BlueBeard.java
+++ app/src/main/java/com/poh/base/Hero_BlueBeard.java
@@ -5,19 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
+
 public class Hero_BlueBeard extends AppCompatActivity {
+    Firebase myFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_layout);
 
-        ImageView heroImage = (ImageView) findViewById(R.id.heroImage);
-        heroImage.setImageResource(R.drawable.eizo_test);
-        }
+        final TextView heroText = (TextView) findViewById(R.id.heroText);
+
+        Firebase.setAndroidContext(getApplicationContext());
+
+        myFirebase = new Firebase("https://planet-of-heroes-base.firebaseio.com/Text");
+
+        myFirebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String myChildText = dataSnapshot.getValue(String.class);
+                heroText.setText(myChildText);
+            }
+
+            @Override
+            public void onCancelled(com.firebase.client.FirebaseError firebaseError) {
+                heroText.setText("Error found");
+            }
+        });
+    }
+
 
     public void backBtnClick(View v){
         this.finish();
